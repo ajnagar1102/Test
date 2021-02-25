ed656428b0b627688225cf4dc8ed997c3ff873c5
Ajay Nagar <ajaynagar.9893@gmail.com>
2021-02-26 01:31:11 +0530

    Add project files.

---
+++ MyShellApp/MyShellApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MyShellApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyShellApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MyShellApp/MyShellApp/Views/Page1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyShellApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Shell.Current.FlyoutIsPresented = true;
+        }
+
+        private void Ctgrylist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            subGrid.IsVisible = true;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            subGrid.IsVisible = false;
+        }
+
+        private void SubCtgrylist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            (sender as ListView).SelectedItem = null;
+            subGrid.IsVisible = false;
+        }
+    }
+}
